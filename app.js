f3022d6e32e06472aaea91c89d7f36a7d253948e
WgtnDan <daniel@danandstorm.co.nz>
2020-12-23 17:09:33 +1100

    add in uncal values

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ const meterSpecificGravity = meter.createValueObserver("specificGravity", {
     labelKeys: ["tiltColour"],
     description: "Records specificGravity of Tilt"
 });
+const meterUncalTemperature = meter.createValueObserver("uncalTemperature", {
+  monotonic: false,
+  labelKeys: ["tiltColour"],
+  description: "Records Uncalibrated temperature of Tilt"
+});
+const meterUncalSpecificGravity = meter.createValueObserver("uncalSpecificGravity", {
+  monotonic: false,
+  labelKeys: ["tiltColour"],
+  description: "Records Uncalibrated specificGravity of Tilt"
+});
 
 
 let countReadings = 0;
@@ -54,19 +64,25 @@ scanner.onadvertisement = (advertisement) => {
     
     const temperature = dataParser.temperatureCelsius(beacon);
     const specificGravity = dataParser.specificGravity(beacon);
+    const uncalTemperature = dataParser.uncalTemperatureCelsius(beacon);
+    const uncalSpecificGravity = dataParser.uncalSpecificGravity(beacon);
     const colour = dataParser.getTiltColour(beacon.uuid);
 
     countIBeacon.add(1, {tiltColour: colour});
     meterTemperature.bind({tiltColour: colour}).update(temperature);
     meterSpecificGravity.bind({tiltColour: colour}).update(specificGravity);
+    meterUncalTemperature.bind({tiltColour: colour}).update(uncalTemperature);
+    meterUncalSpecificGravity.bind({tiltColour: colour}).update(uncalSpecificGravity);
 
-    console.log("count: " + countReadings);
-    console.log("temp:" + temperature);
-    console.log("SG: " + specificGravity);
-    console.log("colour: " + colour); 
-    console.log("uuid: " + beacon.uuid);
-    console.log("rssi: " + beacon.rssi);
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    console.log("count: " + countReadings,
+                ", temp:" + temperature,
+                ", SG: " + specificGravity,
+                ", uncalTemp:" + uncalTemperature,
+                ", uncalSG: " + uncalSpecificGravity,
+                ", colour: " + colour,
+                ", uuid: " + beacon.uuid,
+                ", rssi: " + beacon.rssi);
+    
 };
 
 scanner.startScan().then(() => {
--- dataParser.js
+++ dataParser.js
@@ -15,10 +15,15 @@ const calibrationPoints = loadCalibrationPoints();
 module.exports.temperatureCelsius = (bleacon) => {
   return getCalibratedTemperature((bleacon.major - 32) / 1.8);
 }
-
+module.exports.uncalTemperatureCelsius = (bleacon) => {
+  return (bleacon.major - 32) / 1.8;
+}
 
 module.exports.specificGravity = (bleacon) => {
   return getCalibratedGravity(bleacon.minor / 1000);
+}
+module.exports.uncalSpecificGravity = (bleacon) => {
+  return bleacon.minor / 1000;
 }
 
 
