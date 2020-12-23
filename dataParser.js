c1c761ba4edf559b1f02a5a6b2cc444423b4029d
WgtnDan <daniel@danandstorm.co.nz>
2020-12-23 17:00:14 +1100

    Adding in Calibration Points

--- dataParser.js
+++ dataParser.js
@@ -9,13 +9,16 @@ colourMap.set("A495BB60-C5B1-4B44-B512-1370F02D74DE", "Blue");
 colourMap.set("A495BB70-C5B1-4B44-B512-1370F02D74DE", "Yellow");
 colourMap.set("A495BB80-C5B1-4B44-B512-1370F02D74DE", "Pink");
 
+const fs = require('fs');
+const calibrationPoints = loadCalibrationPoints();
+
 module.exports.temperatureCelsius = (bleacon) => {
-  return (bleacon.major - 32) / 1.8;
+  return getCalibratedTemperature((bleacon.major - 32) / 1.8);
 }
 
 
 module.exports.specificGravity = (bleacon) => {
-  return bleacon.minor / 1000;
+  return getCalibratedGravity(bleacon.minor / 1000);
 }
 
 
@@ -47,3 +50,53 @@ module.exports.getTiltColour = (uuid) => {
     }
 }
 
+function getCalibratedGravity (SG){
+
+  var unCalSGPointsArray = calibrationPoints.gravity.uncalibrated 
+  var actualSGPointsArray= calibrationPoints.gravity.calibrated 
+
+  //temporary array for finding correct x and y values
+  var unCalSGPointsTempArray = loadCalibrationPoints().gravity.uncalibrated 
+  //add current value to calibration point list
+  unCalSGPointsTempArray.push(SG);
+  //sort list lowest to highest
+  unCalSGPointsTempArray.sort(function(a, b){return a-b;});
+  // console.log(unCalSGPointsArray);
+  // console.log(unCalSGPointsTempArray);
+  var indexSG = unCalSGPointsTempArray.indexOf(SG);
+  var calSG = linearInterpolation (Number(SG), Number(unCalSGPointsArray[indexSG-1]), Number(actualSGPointsArray[indexSG-1]), Number(unCalSGPointsArray[indexSG]), Number(actualSGPointsArray[indexSG]));
+  return String(Number(calSG).toFixed(4));
+}
+function getCalibratedTemperature (temp){
+
+  var unCalTempPointsArray = calibrationPoints.temperature.uncalibrated 
+  // console.log(unCalTempPointsArray);
+  var actualTempPointsArray= calibrationPoints.temperature.calibrated 
+
+  //temporary array for finding correct x and y values
+  var unCalTempPointsTempArray = loadCalibrationPoints().temperature.uncalibrated 
+  //add current value to calibration point list
+  unCalTempPointsTempArray.push(temp);
+  //sort list lowest to highest
+  unCalTempPointsTempArray.sort(function(a, b){return a-b;});
+  // console.log(unCalTempPointsTempArray);
+  var indextemp = unCalTempPointsTempArray.indexOf(temp)-1;
+  // console.log(unCalTempPointsArray);
+  var caltemp = linearInterpolation(Number(temp), Number(unCalTempPointsArray[indextemp-1]), Number(actualTempPointsArray[indextemp-1]), Number(unCalTempPointsArray[indextemp]), Number(actualTempPointsArray[indextemp]));
+  return String(Number(caltemp).toFixed(2));
+}
+
+
+function linearInterpolation (x, x0, y0, x1, y1) {
+  // console.log(x + ", " + x0 + ", " + y0 + ", " + x1 + ", " + y1);
+  var a = (y1 - y0) / (x1 - x0);
+  // console.log("a:" + a);
+  var b = -a * x0 + y0;
+  // console.log("b:" + b);
+  return a * x + b;
+}
+
+function loadCalibrationPoints (){
+  let rawdata = fs.readFileSync('calibrationPoints.json');
+  return JSON.parse(rawdata); 
+}
