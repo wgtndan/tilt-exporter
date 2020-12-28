7d3bbe348ab2b192ec9e8dc7bcaae34403774a34
WgtnDan <daniel@danandstorm.co.nz>
2020-12-28 19:57:12 +1100

    Drop console logging

--- logger.js
+++ logger.js
@@ -4,7 +4,7 @@ const LokiTransport = require("winston-loki");
 const logger = createLogger({
     levels: config.syslog.levels,
     transports: [
-        new transports.Console(),
+        // new transports.Console(),
         // new transports.File({ filename: 'combined.log' }),
         new LokiTransport({
             host: "http://rpi2.local:3100",
