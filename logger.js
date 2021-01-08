0ebb5e5bebe9d8f1ceaa5e298edc0f629cf51c9d
WgtnDan <daniel@danandstorm.co.nz>
2021-01-08 15:40:51 +1100

    .

--- logger.js
+++ logger.js
@@ -5,7 +5,7 @@ const logger = createLogger({
     levels: config.syslog.levels,
     transports: [
         new transports.Console(),
-        // new transports.File({ filename: 'combined.log' }),
+        new transports.File({ filename: 'combined.log' }),
         new LokiTransport({
             host: "http://rpi2.local:3100",
             json: true,
