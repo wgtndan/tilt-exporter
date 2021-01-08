8db247eae19e370d94a51d5d120c3de1db6475b7
WgtnDan <daniel@danandstorm.co.nz>
2021-01-08 15:42:11 +1100

    .

--- app.js
+++ app.js
@@ -90,8 +90,8 @@ scanner.onadvertisement = (advertisement) => {
     meterUncalTemperature.bind({tiltColour: tiltbeacon.colour, batchLabel: tiltbeacon.batchLabel}).update(tiltbeacon.uncalTemperature);
     meterUncalSpecificGravity.bind({tiltColour: tiltbeacon.colour, batchLabel: tiltbeacon.batchLabel}).update(tiltbeacon.uncalSpecificGravity);
 
-    logger.info({message: "Valid Beacon Processed", labels:{'tilt_exporter_colour': tiltbeacon.colour, 'tilt_exporter_batchLanel': tiltbeacon.batchLabel, 'tilt_exporter_uuid':tiltbeacon.uuid},  tiltbeacon});
-    // logger.info({message: "Valid Beacon Processed", labels:{'tilt_exporter_colour': tiltbeacon.colour, 'tilt_exporter_batchLanel': tiltbeacon.batchLabel, 'tilt_exporter_uuid':tiltbeacon.uuid, 'temperature':tiltbeacon.temperature, 'specificGravity':tiltbeacon.specificGravity}});
+    // logger.info({message: "Valid Beacon Processed", labels:{'tilt_exporter_colour': tiltbeacon.colour, 'tilt_exporter_batchLanel': tiltbeacon.batchLabel, 'tilt_exporter_uuid':tiltbeacon.uuid},  tiltbeacon});
+    logger.info({message: "Valid Beacon Processed", labels:{'tilt_exporter_colour': tiltbeacon.colour, 'tilt_exporter_batchLanel': tiltbeacon.batchLabel, 'tilt_exporter_uuid':tiltbeacon.uuid, 'temperature':tiltbeacon.temperature, 'specificGravity':tiltbeacon.specificGravity}});
 };
 
 scanner.startScan().then(() => {
